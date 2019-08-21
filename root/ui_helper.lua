470be749e5d1c2228599209f0490bde7606781f5
HomePCFrom\St0l3n_ID <st0l3nid@yahoo.com>
2019-08-21 19:45:53 +0200

    Update ui_helper
    Working with exact ui table format api provides (test table included)

--- root/ui_helper.lua
+++ root/ui_helper.lua
@@ -1,37 +1,58 @@
-
--- Add custom xml       --- REWORK FOR XML PATH ?
-function addInterfaceElement(tUIElement)
-
-    tXML = getXmlTable()
-
-    for gEl, gElTable in tUIElement do --go through given table and extract main components
-        for element, table in tXML do  --look at main components of current ui
-            if element.tag = tUIElement[gEl].tag then -- append the current ui with the new components
-                tables.push(element.children, table)
-            end
-        end
-    end
+txmltest={
+    {
+        tag="HorizontalLayout",
+        attributes={
+            height=200,
+            width=1000,
+            color="rgba(0,0,0,0.7)",
+        },
+        children={
+            {
+                tag="Text",
+                attributes={
+                    fontSize=100,
+                    color="red",
+                },
+                value="Example",
+            },
+            {
+                tag="Text",
+                id="hi",
+                attributes={
+                    text="Message",
+                    fontSize=100,
+                    color="blue",
+                },
+            },
+        }
+    }
+}
+function getXmlTable()
+  return txmltest
 end
 
-
-tPathFaction = {}
 
 --adds the new UI as children to the first element with matching ID
 -- tNewUi: ui code as lua table
 -- sID: unique ID of interface element
--- xmlUI leave empty, or provide current wip ui
-function addUIElementAtID(tNewUi, sID, xmlUi) 
-    tFinalNode = {}
-    for _, v in (xmlUi or getXmlTable()) do
+-- xmlUI leave empty, or provide current wip-ui
+function addUIElementAtID(tNewUi, sID, tXmlUi) 
+    for _, v in ipairs(tXmlUi or getXmlTable()) do  
         if(v.id == sID) then
-            tables.push(v.children, tNewUi) 
-            return true
+            v.children = v.children or {}
+            table.insert(v.children, tNewUi) 
+
+            io.write(v.tag..v.id..v.children[1].tag)
+
+            return true, tXmlUI
         end 
 
-        if(type(v) == 'table') then
-            return addUIElementAtID(tNewUi, sID, v)
+        if(v.children) then
+            return addUIElementAtID(tNewUi, sID, v.children)
         end
     end
 
-    return false
+    return false, tXmlUI
 end
+
+addUIElementAtID({tag="te3xt", lol="fufufu"}, "hi")
