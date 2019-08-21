975e508e26bf588a83cc74465229f1a2cc7830fc
St0l3nID <st0l3nid@yahoo.com>
2019-08-21 17:03:42 +0200

    ui idea
    before i forget that
    dummdummdumm
    dudumm
    dummdummdumm
    BEFORE I FORGET TTHAAAT

--- root/ui_helper.lua
+++ root/ui_helper.lua
@@ -16,11 +16,22 @@ end
 
 tPathFaction = {}
 
-
-function addUIElementAtPath(tUIE, ...) --the vararg is used for path nodes
+--adds the new UI as children to the first element with matching ID
+-- tNewUi: ui code as lua table
+-- sID: unique ID of interface element
+-- xmlUI leave empty, or provide current wip ui
+function addUIElementAtID(tNewUi, sID, xmlUi) 
     tFinalNode = {}
-    for _, tNode do
-        tNode.tag 
+    for _, v in (xmlUi or getXmlTable()) do
+        if(v.id == sID) then
+            tables.push(v.children, tNewUi) 
+            return true
+        end 
+
+        if(type(v) == 'table') then
+            return addUIElementAtID(tNewUi, sID, v)
+        end
     end
-    tables.push(tNode, tUIE) 
+
+    return false
 end
