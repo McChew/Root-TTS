db84bba61188b1fdced2e713a291c2df4b1b8095
St0l3n_ID <st0l3nid@yahoo.com>
2019-12-02 20:03:47 +0100

    DynamicUI

--- root/buttonsv2.lua
+++
@@ -1,8 +0,0 @@
-buttons = {"Marquise", "Eyrie", "Woodland", "Vagabond",
- "Vagabond", "Riverfolk", "Lizard", "Corvid", "Duchy",
-  "Fangus", "rBotMarquise", "MechMarquise", "rBotEyrie",
-  "ElecEyrie1", "EleAEyrie1", "rBotWoodland1", "AutoWoodland1",
-   "rBotVagabond2", "rBotVagabond1", "RoboVagabond2", "RoboVagabond1",
-   "OverRiverfolk1", "ClocLizard1", "VagaArbiter", "VagaRanger",
-    "VagaScoundrel", "VagaThief", "VagaTinker", "VagaVagrant",
-     "Arachnid1", "WeeklyCroak1", "Boars1"}
